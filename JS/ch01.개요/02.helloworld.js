742aa7322bc165662d2688d60253612780412469
Yunyul <gmldnd93@gmail.com>
2024-07-15 12:37:03 +0900

    JavaScript

---
+++ JS/ch01.개요/02.helloworld.js
@@ -0,0 +1,15 @@
+// 자바 스크립트의 주석(Comment)
+console. log('hello world!!!');
+
+/*
+* 반복문 - 여러중에 걸쳐서 주석을 작성하는 경우
+*/
+
+let sum = 
+0;
+for (let i = 1; i <=1000; i++) {
+	sum +=i;
+}
+console.log('1에서 1000까지의 합은', sum );
+
+// 편하게 주석다는 방법 - 마우스로 선툭해 Ctrl + /
